aa199fae17f0568910c3b98b75eb02e7df58dc42
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-05-09 23:02:24 +0530

    feat: add user input validation schema using Zod

---
+++ types.ts
@@ -0,0 +1,7 @@
+import zod from "zod";
+
+export const userInputValidation = zod.object({
+    email: zod.string().email(),
+    password: zod.string().min(8),
+    name: zod.string().min(3).max(50),
+})
