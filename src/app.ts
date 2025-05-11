fa99c9b9a8c93d311b4d5c095fe84df9e0da2151
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-05-11 23:16:14 +0530

    feat: configure CORS to allow requests from localhost with credentials

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ import cors from "cors";
 const app = express();
 
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:8000",
+    credentials: true,
+}))
 app.use(express.json());
 app.use(cookieParser())
 
