ef269b5e968706a5b735fe9d93914ee58f08c5e3
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-05-17 23:08:15 +0530

    feat: implement submitReview functionality to allow mentees to submit reviews for mentors

--- src/controllers/mentor.controller.ts
+++ src/controllers/mentor.controller.ts
@@ -61,3 +61,84 @@ export const searchMentors = async(req:Request,res:Response,next:NextFunction)=>
   })
 
 }
+
+
+export const submitReview = async(req:Request,res:Response,next:NextFunction)=>{
+  try{
+    const {mentorId} = req.params
+    const {rating,comment} = req.body 
+    const user = req.user
+
+    if(user?.role !== "MENTEE"){
+       res.status(403).json({
+        message: "Only mentee can submit review"
+      })
+      return
+    }
+    // Check if the mentor exists
+    const mentor = await prisma.user.findUnique({
+      where: {
+        id: mentorId
+      },
+    include: {mentorProfile: true}
+    
+    })  
+    if(!mentor){
+      res.status(404).json({
+        message: "Mentor not found"
+      })
+      return
+    }
+  //Check for existing review and rating
+  const existingReview = await prisma.review.findFirst({
+    where:{
+      fromId:user?.id,
+    }
+  })
+  if(existingReview){
+     res.status(400).json({
+      message: "You have already submitted a review"
+    })
+    return
+  }
+
+  // Create a new review
+  const review = await prisma.review.create({
+    data:{
+      rating,
+      comment,
+      fromId:user?.id,
+      toId:mentorId
+    }
+  })
+
+  // Update the mentor's rating
+ const reviews  = await prisma.review.findMany({
+  where:{
+    toId:mentorId
+  }
+})
+
+const avgRating  = reviews.reduce((sum,r)=>sum + r.rating,0) / reviews.length
+  await prisma.mentorProfile.update({
+    where:{
+      id:mentorId
+    },
+    data:{
+      rating:avgRating
+    }
+  })
+res.status(201).json({
+  message: "Review submitted successfully",
+  review,
+  rating:avgRating
+})
+  }catch(error){ 
+    console.log(error)
+     res.status(500).json({
+      message: "Internal server error",
+      error: error
+    })
+    return
+  }
+}
--- src/routes/mentor.routes.ts
+++ src/routes/mentor.routes.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import { searchMentors } from '../controllers/mentor.controller'
+import { searchMentors, submitReview } from '../controllers/mentor.controller'
+import { authMiddleware } from '../middlewares/auth.middleware'
 
 
 const router = express.Router()
 
 
 router.get('/search',searchMentors)
-
+router.post('/:mentorId/review',authMiddleware as any, submitReview)
 
 export default router
 
