69c6e8a9c9e4ebad2311767a060a924a5987bfab
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-05-20 17:19:23 +0530

    feat: add helmet and xss-clean for enhanced security

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,26 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./routes/user.routes"
 import mentorRouter from "./routes/mentor.routes"
+import helmet from "helmet";
 
 const app = express();
 
 
+app.use((req,res,next)=>{
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        res.redirect(`https://${req.headers.host}${req.url}`)
+        return
+    }
+    next();
+})
+app.use(helmet())
 app.use(cors({
     origin: "http://localhost:8000",
     credentials: true,
 }))
 app.use(express.json());
 app.use(cookieParser())
+
 
 
 app.use("/user",userRouter)
